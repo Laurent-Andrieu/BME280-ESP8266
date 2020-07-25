c80821a29ffe2a536babadc93fc87c49b88e3028
Laurent <47950239+Laurent-Andrieu@users.noreply.github.com>
2020-07-25 13:20:06 +0200

    MicroPython source code
    Source Code du projet

---
+++ boot.py
@@ -0,0 +1,57 @@
+import gc
+import sys
+import network
+import webrepl
+import machine
+import ubinascii
+from umqttsimple import MQTTClient
+
+
+SSID = 'Livebox-0270'
+PASS = ''
+mqtt_broker = '192.168.1.100'
+
+version = [str(i)  for i in sys.implementation.version]
+wlan = network.WLAN(network.STA_IF)
+ap = network.WLAN(network.AP_IF)
+ap.active(False)
+networks = wlan.scan()[:][0]
+
+rssi = networks[3]
+py_ver = sys.version
+board = sys.platform
+firmware = sys.implementation.name
+firmware_ver = ".".join(version)
+ap_state = ap.isconnected()
+
+print('\n[Boot]')
+print('\n[i]Python version: %s'% py_ver,
+      '\n[i]board name: %s'% board,
+      '\n[i]firmware: %s'% firmware,
+      '\n[i]version: %s'% firmware_ver)
+print('[i]AP state: %s'% ap_state)
+
+if b'%s'%SSID in networks:
+    print('[i]RSSI: {}db'.format(networks[3]))
+    
+if not wlan.isconnected():
+    print('[+]Connecting to Wifi...\n')
+    wlan.active(True)
+    wlan.connect(SSID, PASS)
+    while not wlan.isconnected():
+        pass
+webrepl.start()
+gc.collect()
+print('\n[i]Memory free:', gc.mem_free())
+
+(ip, netmask, gateway, dns) = wlan.ifconfig()
+mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
+
+wlan_data = {'IP': ip, 'Netmask': netmask, 'Gateway': gateway, 'DNS': dns, 'MAC': mac}
+if not wlan.isconnected():
+    print('[i]Not Connected to Wifi Network, please check SSID & password or Wifi params')
+else:
+    for k, v in wlan_data.items():
+        print('[i]{}: '.format(k), v)
+
+import main
---
+++ main.py
@@ -0,0 +1,80 @@
+import gc
+import time
+import BME280
+import machine
+import network
+from ubinascii import hexlify
+from umqttsimple import MQTTClient
+from sys import version, platform, implementation
+
+wlan = network.WLAN(network.STA_IF)
+mqtt_broker = '192.168.1.100'
+client_id = hexlify(machine.unique_id())
+client_id = client_id.decode('utf-8')
+topic = b'sensor/{}'.format(client_id)
+topic_params = topic + b'/params'
+py_version = [str(i)  for i in implementation.version]
+rssi = wlan.scan()[:][0][3]
+py_ver = version
+board = platform
+firmware = implementation.name
+firmware_ver = ".".join(py_version)
+(ip, netmask, gateway, dns) = wlan.ifconfig()
+mac = hexlify(network.WLAN().config('mac'),':').decode()
+
+# Pin D1 & D2
+i2c = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4), freq=10000)
+print('[i]Client id: ', client_id)
+
+def deep_sleep(msecs):
+  #configure RTC.ALARM0 to be able to wake the device
+  rtc = machine.RTC()
+  rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
+  # set RTC.ALARM0 to fire after Xmilliseconds, waking the device
+  rtc.alarm(rtc.ALARM0, msecs)
+  #put the device to sleep
+  machine.deepsleep()
+
+def connect():
+    global client_id, mqtt_broker
+    client = MQTTClient(client_id, mqtt_broker)
+    client.connect()
+    print('[i]Connected to %s MQTT broker' % (mqtt_broker))
+    return client
+
+def reconnect():
+    print('Failed to connect to MQTT broker. Reconnecting...')
+    time.sleep(10)
+    machine.reset()
+
+try:
+    client = connect()
+except OSError as e:
+    reconnect()
+finally:
+    client.check_msg()
+    params = b'{},{},{},{},{},{},{},{},{},{},{}'.format(client_id, py_ver, firmware,
+                                                        firmware_ver, gc.mem_free(), rssi,
+                                                        ip, mac, gateway,
+                                                        dns, netmask)
+    client.publish(topic_params, params)
+while True:
+    bme = BME280.BME280(i2c=i2c)
+    temp = bme.temperature
+    hum = bme.humidity
+    pres = bme.pressure
+    networks = wlan.scan()[:][0]
+    rssi = networks[3]
+    try:
+        msg = b'{},{},{},{},{}'.format(client_id,rssi,temp,hum,pres)
+        client.publish(topic, msg)
+        last_message = time.time()
+        print('\nRSSI: ', rssi)
+        print('Temperature: ', temp)
+        print('Humidity: ', hum)
+        print('Pressure: ', pres)
+        #5min pause
+        time.sleep(10)
+        deep_sleep(300000)
+    except OSError as e:
+        reconnect()
